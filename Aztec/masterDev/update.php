5d32c79d71b3616b4da4509d0519fb8edc4564e9
Paul St. Denis <pstdenis@huey-tll.labs.stonybrook.edu>
2021-09-15 17:49:41 -0400

    cleaned a bit

--- Aztec/masterDev/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("spreadsheet.php");
-require("group.php");
-$group = new Group();
-$line= $group->group+1;
-$spreadsheet=new Spreadsheet("1vkRW7B33edqK_tlkcMFeVsaZowG_7PnjAwBkb2LN9n8");
-$values=$spreadsheet->updateRange("Game States!C$line",[[$_POST['val']]]);
-print_r($_POST);
