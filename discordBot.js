3cb90aff28b7f388c0990f0a3c66246630ad200b
timothyhsu8 <timothy.hsu@stonybrook.edu>
2022-06-15 08:30:47 -0400

    Greatly simplified DiscordBot class, only requires channels as input now.

--- discordBot.js
+++ discordBot.js
@@ -1,68 +1,15 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 var XMLHttpRequest = require('xhr2');
 
 class DiscordBot {
-    constructor(clientId, guildId, token, channels) {
-        this.clientId = clientId;
-        this.guildId = guildId;
-        this.token = token;
+    constructor(channels) {
         this.channels = channels;
-        this.init();
-    }
-    
-    // Initializes and runs the discord bot
-    init() {
-        const commands = [{
-            name: 'ping',
-            description: 'Replies with Pong!'
-          }, {
-              name: 'pog',
-              description: 'Replies with Pog!'
-          }]; 
-
-        const rest = new REST({ version: '9' }).setToken(this.token);
-
-        (async () => {
-            try {
-            console.log('Started refreshing application (/) commands.');
-        
-            await rest.put(
-                Routes.applicationGuildCommands(this.clientId, this.guildId),
-                { body: commands },
-            );
-        
-            console.log('Successfully reloaded application (/) commands.');
-            } catch (error) {
-            console.error(error);
-            }
-        })();
-        
-        client.once('ready', () => {
-            console.log(`Logged in as ${client.user.tag}`)
-        })
-    
-        client.on('interactionCreate', async interaction => {
-            if (!interaction.isCommand()) return;
-        
-            if (interaction.commandName === 'ping') {
-            await interaction.reply('Pong!');
-            }
-    
-        });
-    
-        client.login(this.token);
     }
 
     // Sends notification to a specific channel 
     sendNotif(channel, message) {
-        console.log({channel, message});
-        
+        // Find channel URL for the requested channel name
         for (const ch of this.channels)
             if (ch.name === channel) {
                 var channelURL = ch.url;            
@@ -75,7 +22,6 @@ class DiscordBot {
 
         const content = message;
         let username, avatar_url;
-        
 
         if (channel === "aztecs") {
             username = 'Aztec Messenger';
