9cb2d0c9c49a87858dd62322a8c83a06398fd2e6
someguy2 <nathanchan4121@gmail.com>
2022-12-13 14:43:26 -0500

    Automated update to Heroku/Github

--- regex.py
+++ regex.py
@@ -18,7 +18,11 @@ replacements = []
 
 for i in ["Aztecs", "Tlaxcalans", "Spaniards"]:
     for j in ["Loyalty", "Strength", "Wisdom"]:
-        replacements.append({'regex': f'{i}_{j}', 'replace' : f'$factions[{i}]["stats"][{j}]'})
+        replacements.append({'regex': f'{i}_{j}', 'replace' : f'factions[{i}]["stats"][{j}]'})
+
+for i in ["Marina","Alvarado","Aguilar","Garrido","Olid","Moctezuma","Tlacaelel","Cuauhtemoc","Aztec_Priest","Cacamatzin","Pochteca","Xicotencatl_Elder","Xicotencatl_Younger","Maxixcatl", "Cortes"]:
+    for j in ["Loyalty", "Strength", "Wisdom"]:
+        replacements.append({'regex': f'{i}_{j}', 'replace' : f'users[$lookup["{i}"]]["stats"]["{j}"]'})
         
 # Opens file
 twee_in_file = open(sys.argv[1], encoding="utf-8")
