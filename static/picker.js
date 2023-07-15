8f94d8046d1f1912dd482eb080aa4356c052abed
dakota-byte <dakotadevelopercode@gmail.com>
2023-07-15 11:05:11 -0400

    number picker fix on this branch

--- static/picker.js
+++ static/picker.js
@@ -1,57 +1,7 @@
-class valueChanger {
-
-    constructor(currentValue) {
-        this.currentValue = currentValue
-        this.maxValue = currentValue;
-        this.statsArray = ["Strength", "Loyalty", "Wisdom"];
-    }
-
-    change(direction, $currentInput) {
-        var spinValue = parseInt($currentInput.val());
-        var changed = false;
-        var testValue = this.currentValue - direction;
-        let accumStats = this.statsArray.reduce(
-            (accumulator, currentValue) => {console.log(parseInt($(`#${currentValue}`).val())); return accumulator + parseInt($(`#${currentValue}`).val()) },
-            0
-        );
-        //console.log(accumStats);
-        if (!(testValue < 0 || testValue > this.maxValue || spinValue < 0)) {
-            
-            this.currentValue = this.maxValue - accumStats;
-            $("#currentValue").html(this.currentValue);
-
-            changed = true;
-            disableButtons();
-        }
-        return changed;
-    }
-}
-
-
-var valueChange = new valueChanger(20);
-
-function dialog(text) {
-    var dc = $("#dialog-confirm")
-    dc.html(text);
-    dc.dialog({
-        resizable: false,
-        height: "auto",
-        width: 400,
-        modal: true,
-        buttons: {
-
-            Cancel: function () {
-                $(this).dialog("close");
-            }
-        }
-    });
-}
-
 function statPickerInit() {
     var statNames = ["Strength", "Wisdom", "Loyalty"]
     var userId = SugarCube.State.getVar("$userId");
     var user = SugarCube.State.getVar("$users")[userId];
-    var role = user["role"];
 
     let stats = SugarCube.State.variables.users[userId]["stats"];
     if (stats) {
@@ -89,80 +39,47 @@ function statPickerInit() {
         "class": "show"
     }).append([pointsLeftLabel,pointsLeft, container, submitButton, dialog])
 
+    
+
     setTimeout(() => {
         $('#statsPicker').empty().append(out)
-        //makeRoleStats()
 
+        
         $("#submitButton").on("click", submitStats)
         $('#stats input[type="number"]').niceNumber({
             onIncrement: ($currentInput, amount, settings) => changeHelper($currentInput, amount, settings, 1),
             onDecrement: ($currentInput, amount, settings) => changeHelper($currentInput, amount, settings, -1),
         });
+
         disableButtons();
+        
     }, 0);
     return ""
 }
 
-function toggleHide() {
-    $('.show').addClass('temp')
-    $('.show').removeClass('show')
-
-    $('.hide').addClass('show')
-    $('.hide').removeClass('hide')
-    $('.temp').addClass('hide')
-}
-
-function submitStats() {
-    var niceNumber = $(".nice-number input");
-    var stats = {}
-    niceNumber.each((index, item) => {
-        var jqItem = $(item);
-        var statType = $(item).attr("id");
-        var statValue = $(item).val();
-        stats[statType] = parseInt(statValue)
-    })
-
-    if (valueChange.currentValue != 0) {
-        dialog(`You still have points to assign`)
-    } else {
-        toggleHide()
-        makeRoleStats(stats)
-    }
-}
-
-function disableButtons() {
-    var total = 0;
-    $('.nice-number').each(function () {
-        var value = $($(this).children()[1]).val()
-        var minus = $($(this).children()[0]);
-        minus.prop("disabled", false);
-        if (value == 0) {
-            minus.prop("disabled", true);
-        }
-
-        //
-        //  
-        //$( this) .attr("disabled","disabled");
-    })
-}
-
-
 function changeHelper($currentInput, amount, settings, direction) {
-    if (amount == 0) {
-        return;
-    }
-
-    if (!valueChange.change(direction, $currentInput)) {
+    if (amount < 0) {
         $currentInput.val($currentInput.val() - direction);
-    } else {
-
+        return
     }
 
-    // if (amount >= 100) {
-    //     $currentInput.classList.add('more-than-100');
-    // } else {
-    //     $currentInput.classList.remove('more-than-100');
-    // }
+    var currentValue = parseInt($("#currentValue").text(), 10);
+    var updatedValue = currentValue - direction;
+    if (updatedValue < 0 || updatedValue > 20) {
+        $currentInput.val($currentInput.val() - direction);
+        return
+    }
+
+    $("#currentValue").html(updatedValue);
+
+    var element = document.getElementById($currentInput[0].id);
+    var parentElement = element.parentNode;
+    var minus = parentElement.firstElementChild;
+    if (amount > 0) {
+        minus.disabled = false;
+    } else {
+        minus.disabled = true;
+    }
 }
 
 function makeStats(item, container, value = 0) {
@@ -175,15 +92,61 @@ function makeStats(item, container, value = 0) {
         "value": value,
         "disabled": true
     });
-    // htm+=label[0].outerHTML+picker[0].outerHTML;
     var div = $("<div/>", {});
     container.append(div.append([label, picker]));
     $('.statsInput').on("focus", function() {
         console.log($(this))
         $(this).blur()
     })
+}
 
-    
+function submitStats() {
+    var niceNumber = $(".nice-number input");
+    var stats = {}
+    niceNumber.each((index, item) => {
+        var statType = $(item).attr("id");
+        var statValue = $(item).val();
+        stats[statType] = parseInt(statValue)
+    })
+    var currentValue = parseInt($("#currentValue").text(), 10);
+    if (currentValue != 0) {
+        dialog(`You still have points to assign`)
+    } else {
+        toggleHide()
+        makeRoleStats(stats)
+    }
+}
+
+function disableButtons() {
+    $('.nice-number').each(function () {
+        var value = $($(this).children()[1]).val()
+        var minus = $($(this).children()[0]);
+        minus.prop("disabled", true);
+    })
+}
+
+function dialog(text) {
+    var dc = $("#dialog-confirm")
+    dc.html(text);
+    dc.dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+
+function toggleHide() {
+    $('.show').addClass('temp')
+    $('.show').removeClass('show')
+    $('.hide').addClass('show')
+    $('.hide').removeClass('hide')
+    $('.temp').addClass('hide')
 }
 
 jsLoaded = true
