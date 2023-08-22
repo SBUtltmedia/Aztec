dda8c52a17805533f79f5ecb79a3d9fb3bf0e3e4
AlmondBread <pstdenis@gmail.com>
2023-08-22 17:05:12 -0400

    Automated update to Heroku/Github

--- syncToHeroku.js
+++ syncToHeroku.js
@@ -4,7 +4,9 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const configObj = require('./config.json')
 
-const app =configObj.serverconf.fileName;
+
+
+const app = configObj.serverconf.fileName;
 if (!app) {
     console.log("Name argument required. Usage: node .\\createHerokuInstances.js startingAppId endingAppId");
     exit(0);
@@ -18,15 +20,15 @@ let commands = [
     `git commit -m "Automated update to Heroku/Github"`
 ]
 
-for (let i = startingAppId; i <= endingAppId ; i++) {
-    let clientId = configObj.channelconf[i-1].clientId;
-    let configVars = {...configObj.channelconf[i-1], ...configObj.serverconf};  // Combine channelconf and serverconf
+for (let i = startingAppId; i <= endingAppId; i++) {
+    let clientId = configObj.channelconf[i - 1].clientId;
+    let configVars = { ...configObj.channelconf[i - 1], ...configObj.serverconf };  // Combine channelconf and serverconf
     commands.push(`git push -f https://git.heroku.com/${app}-${i}.git HEAD:master`);
-    commands.push(`heroku config:set -a ${app}-${i} PROCFILE=Procfile`) 
-    
+    commands.push(`heroku config:set -a ${app}-${i} PROCFILE=Procfile`)
+
     // Add custom config variables
     let redirectURL = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(`https://${app}-${i}.herokuapp.com`)}&response_type=code&scope=identify%20guilds.members.read%20guilds`.replace(/&/g, '"&"');
-    console.log({redirectURL});
+    console.log({ redirectURL });
     configVars['redirectURL'] = redirectURL;
     configVars['herokuURL'] = `https://${app}-${i}.herokuapp.com`;
     configVars['appIndex'] = i;
@@ -41,15 +43,18 @@ for (let i = startingAppId; i <= endingAppId ; i++) {
     delete configVars['discordChannels']
 
     for (let key of Object.keys(configVars)) {
-        let command = `heroku config:set -a ${app}-${i} ${key}=${configVars[key]}`;
-        commands.push(command);
-       console.log(`${key}=${configVars[key]}`);
+        let remoteVal = execSync(`heroku config:get -a ${app}-${i} ${key}`).toString();
+        if (configVars[key] != remoteVal) {
+            let command = `heroku config:set -a ${app}-${i} ${key}=${configVars[key]}`;
+            commands.push(command);
+            console.log(`${key}=${configVars[key]}`);
+        }
     }
 
     for (let command of commands) {
         try {
             execSync(command, console.log);
-        } catch(err) {}
+        } catch (err) { }
         // console.log(commands);
     }
     commands = [];
